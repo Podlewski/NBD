a458dacfa88c4605f4c71a528e5b84f9ed2bd1d2
210182 <210182@edu.p.lodz.pl>
2019-05-18 11:03:45 +0200

    made fill script use ip of default docker machine

--- Cluster/fill.sh
+++ Cluster/fill.sh
@@ -1,4 +1,4 @@
 #!/bin/bash 
 
-mongoimport --jsonArray --host 192.168.99.101:27019 --db diabetes --collection collection --file data_10.json
-docker exec -it mongos1 bash -c "echo -e 'db.createUser({user: \"admi\", pwd: \"abc123\!\", roles: [{ role: \"root\", db: \"admin\" }]})' | mongo"
+mongoimport --jsonArray --host $(docker-machine ip default):27019 --db diabetes --collection collection --file data_10.json
+docker exec -it mongos1 bash -c "echo -e 'db.createUser({user: \"admin\", pwd: \"abc123\!\", roles: [{ role: \"root\", db: \"admin\" }]})' | mongo"
