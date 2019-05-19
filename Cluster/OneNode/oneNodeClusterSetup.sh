b3492aab5cead188a1cdd870d8a5cefa4132ddf2
210182 <210182@edu.p.lodz.pl>
2019-05-19 21:35:16 +0200

    created separate compose file for one node

--- Cluster/OneNode/oneNodeClusterSetup.sh
+++ Cluster/OneNode/oneNodeClusterSetup.sh
@@ -3,14 +3,10 @@
 echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ initiating config replica set"
 docker exec -i mongocfg1 bash -c "echo 'rs.initiate({_id: \"mongors1conf\",configsvr: true, members: [{ _id : 0, host : \"mongocfg1\" },{ _id : 1, host : \"mongocfg2\" }]})' | mongo"
 echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ initiating shard1 replica set"
-docker exec -i mongors1n1 bash -c "echo 'rs.initiate({_id : \"mongors1\", members: [{ _id : 0, host : \"mongors1n1\" },{ _id : 1, host : \"mongors1n2\" }]})' | mongo"
-#echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ initiating shard2 replica set"
-#docker exec -i mongors2n1 bash -c "echo 'rs.initiate({_id : \"mongors2\", members: [{ _id : 0, host : \"mongors2n1\" },{ _id : 1, host : \"mongors2n2\" }]})' | mongo"
+docker exec -i mongors1n1 bash -c "echo 'rs.initiate({_id : \"mongors1\", members: [{ _id : 0, host : \"mongors1n1\" },{ _id : 1, host : \"mongors1n2\" },{ _id : 2, host : \"mongors1n3\" }]})' | mongo"
 echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ adding shard1"
 docker exec -i mongos1 bash -c "echo 'sh.addShard(\"mongors1/mongors1n1\")' | mongo "
 sleep 2
-#echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ adding shard2"
-#docker exec -i mongos1 bash -c "echo 'sh.addShard(\"mongors2/mongors2n1\")' | mongo "
 echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ creating diabetes db"
 docker exec -i mongors1n1 bash -c "echo 'use diabetes' | mongo"
 sleep 2
