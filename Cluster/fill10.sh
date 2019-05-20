8a6b2a170de75d36293be741fbc250d25cd06a20
Karol Podlewski <210294@edu.p.lodz.pl>
2019-05-20 23:09:50 +0200

    typo

--- Cluster/fill10.sh
+++ Cluster/fill10.sh
@@ -1,4 +1,4 @@
 #!/bin/bash 
 
 mongoimport --jsonArray --host `docker-machine ip default`:27019 --db diabetes --collection collection --file ../DatabaseScripts/data_10.json
-docker exec -it mongos1 bash -c "echo -e 'db.createUser({user: \"admi\", pwd: \"abc123\!\", roles: [{ role: \"root\", db: \"admin\" }]})' | mongo"
+docker exec -it mongos1 bash -c "echo -e 'db.createUser({user: \"admin\", pwd: \"abc123\!\", roles: [{ role: \"root\", db: \"admin\" }]})' | mongo"
