66fa2822c155437cbe2f0048213b641f11a84614
210182 <210182@edu.p.lodz.pl>
2019-06-01 20:32:59 +0200

    modified fill scripts to take host as an argument

--- Cluster/fill.sh
+++ Cluster/fill.sh
@@ -1,4 +1,10 @@
 #!/bin/bash 
 
-mongoimport --jsonArray --host `docker-machine ip default`:27019 --db diabetes --collection collection --file ../DatabaseScripts/data.json
-docker exec -it mongos1 bash -c "echo -e 'db.createUser({user: \"admin\", pwd: \"abc123\!\", roles: [{ role: \"root\", db: \"admin\" }]})' | mongo"
+if [ $# -ne 1 ]
+then
+	echo -e 'Ilegal number of arguments.\n\nUsage:\n fill10.sh {host}\n\nMake sure to be on cluser1 machine!'
+	exit 1
+fi
+
+mongoimport --jsonArray --host $1:27019 --db diabetes --collection collection --file ../DatabaseScripts/data.json
+docker exec -i $(docker ps | grep mongos1 | awk '{print $1;}') bash -c "echo -e 'db.createUser({user: \"admin\", pwd: \"abc123\!\", roles: [{ role: \"root\", db: \"admin\" }]})' | mongo"
